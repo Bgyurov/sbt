d744f770f7dc8bc3492e669a0bd0466f6f9a772e
Boyan Gyurov <49366221+Bgyurov@users.noreply.github.com>
2025-01-04 12:13:21 +0200

    Linux settings

--- crawler/error-crawler.js
+++ crawler/error-crawler.js
@@ -3,15 +3,24 @@ const fs = require("fs");
 const path = require("path");
 
 async function accessSafePage() {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: "/snap/bin/chromium",
+  });
   const page = await browser.newPage();
 
-  await page.setUserAgent("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)");
+  await page.setUserAgent(
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)"
+  );
 
   await page.setCookie({
     name: "userData",
-    value: JSON.stringify({ isTouchable: true, isHeadless: true, isMobileResolution: true }),
-    url: "http://localhost:8080"
+    value: JSON.stringify({
+      isTouchable: true,
+      isHeadless: true,
+      isMobileResolution: true,
+    }),
+    url: "http://localhost:8080",
   });
 
   const url = "http://localhost:8080/money?gclid=invalidgclid";
--- crawler/success-crawler.js
+++ crawler/success-crawler.js
@@ -1,22 +1,27 @@
 const puppeteer = require("puppeteer");
 
 async function accessMoneyPage() {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: "/snap/bin/chromium",
+  });
   const page = await browser.newPage();
 
-  await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36");
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36"
+  );
   await page.setExtraHTTPHeaders({
     Referer: "https://google.com",
   });
 
   const userDataCookie = {
-    name: 'userData',
+    name: "userData",
     value: JSON.stringify({
-      isTouchable: false,  
-      isHeadless: false,   
-      isMobileResolution: false 
+      isTouchable: false,
+      isHeadless: false,
+      isMobileResolution: false,
     }),
-    url: 'http://localhost:8080'
+    url: "http://localhost:8080",
   };
   await page.setCookie(userDataCookie);
 
